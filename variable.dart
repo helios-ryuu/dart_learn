633cbdf6e1f44501a307574708d918ce2f0065b5
Helios <ngotiensy2005@gmail.com>
2024-02-13 12:52:06 +0700

    Update

--- variable.dart
+++ variable.dart
@@ -7,8 +7,8 @@ int main() {
   country = "VN";
   double? salary;
   salary = 2000;
-  bool gender = true;
-  Object height = 175;
+  bool? gender = true;
+  Object? height = 175;
 
   print("Salary: $salary");
   return 0;
