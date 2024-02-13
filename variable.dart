e7b202dccc1c7e7899d3117c7b604a101648690b
Helios <ngotiensy2005@gmail.com>
2024-02-13 12:35:33 +0700

    Modify variable.dart

--- variable.dart
+++ variable.dart
@@ -1,6 +1,15 @@
 int main() {
-  var a = 20;
-  print(a);
+  var year = 2024;
+  String name = "Helios";
+  dynamic age = 19;
+  const String location = "TPHCM";
+  final String country;
+  country = "VN";
+  double? salary;
+  salary = 2000;
+  bool gender = true;
+  Object height = 175;
+
+  print("Salary: $salary");
   return 0;
 }
-
